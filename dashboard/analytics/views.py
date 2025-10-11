3ec0ba520bac423c756baf054fbde938684ac8c8
Saad Muhammad <saad.py0@gmail.com>
2025-10-11 20:30:43 +0530

    Fixed event_generation issue, cleared database. Added visualisation for other tables

--- dashboard/analytics/views.py
+++ dashboard/analytics/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Aggregate
+from .models import Aggregate, EventCount, TopUser
 
 def dashboard(request):
-    data = Aggregate.objects.all().order_by('-total_sales')
-    return render(request, 'analytics/dashboard.html', {'data': data})
+    context = {
+        'data': Aggregate.objects.all().order_by('-total_sales'),
+        'event_counts': EventCount.objects.all().order_by('product_name', 'event_type'),
+        'top_users': TopUser.objects.all().order_by('-total_purchases')[:10]  # Top 10 users
+    }
+    return render(request, 'analytics/dashboard.html', context)
